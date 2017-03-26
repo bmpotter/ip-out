29b658612cd974e1629a73d78c86750c0fa7c1ad
michael dye <m-github@divisive.info>
2017-03-26 15:49:56 -0600

    added ipv4 wait, replaced operational up/down status with link up/down status

--- main.go
+++ main.go
@@ -8,10 +8,21 @@ import (
 	"os"
 	"regexp"
 	"text/tabwriter"
+	"time"
+
+	"syscall"
+
+	// need this to get layer 2 link status from an interface, see https://codereview.appspot.com/105440043/
+	"github.com/vishvananda/netlink"
 )
 
-// DefaultExcludes declares the default interface exclusions from output
-const DefaultExcludes = `^veth|docker|br-|sit`
+const (
+	// ipv4 wait timeout seconds
+	ipv4WaitTimeoutS = 30
+
+	// defaultExcludes declares the default interface exclusions from output
+	defaultExcludes = `^veth|docker|br-|sit`
+)
 
 type address []string
 
@@ -49,7 +60,27 @@ type Interface struct {
 	Addresses address `json:"addresses"`
 }
 
-func interfaces(nameExcludePattern *regexp.Regexp) ([]*Interface, error) {
+func interfaces(nameExcludePattern *regexp.Regexp, ipv4Wait *bool) ([]*Interface, error) {
+
+	gatherAddrs := func(addrs []net.Addr, names *[]string) bool {
+		containsIpv4 := false
+
+		for _, addr := range addrs {
+			addrString := addr.String()
+
+			if ip, _, err := net.ParseCIDR(addrString); err == nil && ip != nil {
+				*names = append(*names, addrString)
+
+				if v4 := ip.To4(); v4 != nil {
+					containsIpv4 = true
+				}
+			}
+
+		}
+
+		return containsIpv4
+	}
+
 	var out []*Interface
 
 	interfaces, err := net.Interfaces()
@@ -58,26 +89,44 @@ func interfaces(nameExcludePattern *regexp.Regexp) ([]*Interface, error) {
 	}
 
 	for _, iface := range interfaces {
-		if nameExcludePattern != nil && nameExcludePattern.MatchString(iface.Name) ||
-			iface.Flags&net.FlagLoopback != 0 {
+		if iface.Flags&net.FlagLoopback != 0 || (nameExcludePattern != nil && nameExcludePattern.MatchString(iface.Name)) {
 			continue
 		}
 
-		// make sure this is always at least empty so it can be iterated over safely
-		addrNames := make([]string, 0)
-
-		addrs, err := iface.Addrs()
-		if err != nil {
+		// TODO: consider making this a tri-state type so we can reflect unknown / unfetchable value
+		var linkUp link
+		var link netlink.Link
+		if link, err = netlink.LinkByName(iface.Name); err != nil {
 			return nil, err
 		}
 
-		for _, addr := range addrs {
-			addrNames = append(addrNames, addr.String())
+		linkUp = (*link.Attrs()).RawFlags&syscall.IFF_RUNNING == uint32(syscall.IFF_RUNNING)
+
+		addrNames := []string{}
+
+		// only bother gathering addresses if link is up
+		if linkUp {
+			addrs, err := iface.Addrs()
+			if err != nil {
+				return nil, err
+			}
+
+			start := time.Now()
+			for {
+				var containsIpv4 bool
+				containsIpv4 = gatherAddrs(addrs, &addrNames)
+
+				if ipv4Wait == nil || !*ipv4Wait || containsIpv4 || time.Now().Sub(start) > time.Duration(ipv4WaitTimeoutS)*time.Second {
+					break
+				}
+
+				time.Sleep(500 * time.Millisecond)
+			}
 		}
 
 		out = append(out, &Interface{
 			Name:      iface.Name,
-			LinkUp:    iface.Flags&net.FlagUp != 0,
+			LinkUp:    linkUp,
 			Addresses: addrNames,
 		})
 	}
@@ -87,11 +136,12 @@ func interfaces(nameExcludePattern *regexp.Regexp) ([]*Interface, error) {
 
 func main() {
 	pp := flag.Bool("pp", false, "Pretty-print output")
+	ipv4Wait := flag.Bool("ipv4-wait", true, fmt.Sprintf("Wait for at least one ipv4 address to be assigned to an interface before reporting up to timeout of %vs", ipv4WaitTimeoutS))
 	flag.Parse()
 
-	excludeRegexp := regexp.MustCompile(DefaultExcludes)
+	excludeRegexp := regexp.MustCompile(defaultExcludes)
 
-	outs, err := interfaces(excludeRegexp)
+	outs, err := interfaces(excludeRegexp, ipv4Wait)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading interfaces: %s\n", err)
 		os.Exit(1)
